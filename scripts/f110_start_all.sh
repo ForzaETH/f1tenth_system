ad232e2eddc235ca6c757dd99b0e80db41aaf47e
nibauman <nicolas.baumann@pbl.ee.ethz.ch>
2024-03-07 10:28:52 +0000

    replace path in scripts

--- scripts/f110_start_all.sh
+++ scripts/f110_start_all.sh
@@ -1,7 +1,7 @@
 echo 'Starting all the racing stuff...'
 
 #Start teleop
-/home/forzapblcar/catkin_ws/src/pbl_f110_system/scripts/f110_start_teleop.sh &
+/home/forzapblcar/catkin_ws/src/f1tenth_system/scripts/f110_start_teleop.sh &
 
 
 #Sleep to guarantee nodes started
@@ -9,6 +9,6 @@ sleep 5
 
 
 #Start SLAM
-/home/forzapblcar/catkin_ws/src/pbl_f110_system/scripts/f110_start_slam.sh &
+/home/forzapblcar/catkin_ws/src/f1tenth_system/scripts/f110_start_slam.sh &
 
 echo 'Done with racing...'
